541dd5076474ac7c64a7fb015da0bcd3d0012266
Ace <121155146+DocterMoose@users.noreply.github.com>
2023-01-25 16:21:49 -0700

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,50 +1,30 @@
--- Made by ! Ace#3790
+--Script Made by ! Ace#3790 --
 
---- Hope you enjoy! :D ---
-
-
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Player = game.Players.LocalPlayer
-local Window = OrionLib:MakeWindow({Name = "Key System", HidePremium = false, IntroEnabled = false, SaveConfig = true, ConfigFolder = "OrionTest"})
-OrionLib:MakeNotification({
-	Name = "Welcome.. Fate awaits you!",
-	Content = "You're Logged in as "..Player.Name.."",
-	Image = "rbxassetid://4483345998",
-	Time = 5
-})
-
-_G.Key = "New"
-_G.KeyInput = "string"
+-- Enjoy :D!! --
 
 
-
-
-
-
-function MakeScriptHub()
-    if game.PlaceId == 5864786637 then
-        local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-        local Window = OrionLib:MakeWindow({Name = "In Another Time UI", HidePremium = false, SaveConfig = true, IntroEnabled = false, IntroText = "AceHub", ConfigFolder = "OrionTest"})
+if game.PlaceId == 5864786637 then
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+    local Window = OrionLib:MakeWindow({Name = "In Another Time [ MONEY UPDATE 🤑 ] UI", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest", IntroEnabled = false, IntroText = "Ace hub"})
+    
     
     --Values
-    _G.NoCoolDown = true
     _G.BypassTS = true
+    _G.InfiniteBlock = true
+    _G.OHHeal = true
+    _G.StandStorage1 = true
+    _G.StandStorage2 = true
+    _G.StandStorage3 = true
+    _G.InfiniteEpitaph = true
+    _G.TheHandPull = true
+    _G.TheHandSlip = true
     _G.LightningSpam = true
-    
-    
-    
-    
-    --Functions
-    
-    function NoCoolDown()
-        while _G.NoCoolDown == true do
-            game:GetService("ReplicatedStorage").Attacks.SpaceRipper:FireServer()
-    
-             game:GetService("ReplicatedStorage").CoolDowns:FireServer("Space Ripper Stingy Eyes","(T)",10)
-            wait(.01)
-        end
-    end
-    
+    _G.SpaceRipper = true
+    _G.TW20SECTS = true
+    _G.WSVisual = true
+    _G.KnifeSpam = true
+    _G.Getarrow = true
+    -- Main functions: [TimeStopImmunity] --
     function BypassTS()
         while _G.BypassTS == true do
             local args = {
@@ -128,166 +108,368 @@ function MakeScriptHub()
             local args = {
                 [1] = game:GetService("Players").LocalPlayer.Character.Head,
                 [2] = false
-            
             }
             
             game:GetService("ReplicatedStorage").Basic.Anchor:FireServer(unpack(args))
             
-            wait(0.0002)
+            wait(.0002)
+        end
+    end
+    -- Main functions: [Troll] --
+    function InfiniteBlock()
+        while _G.InfiniteBlock == true do
             local args = {
                 [1] = true
             }
             
             game:GetService("ReplicatedStorage").Basic.Block:FireServer(unpack(args))
-            -----------------------------------------------------------------
-            local args = {
-                [1] = game:GetService("Players").LocalPlayer.Character.Head,
-                [2] = false
-            }
-            
-            game:GetService("ReplicatedStorage").Basic.Anchor:FireServer(unpack(args))
-            local args = {
-                [1] = game:GetService("Players").LocalPlayer.Character.Torso,
-                [2] = false
-            }
-            
-            game:GetService("ReplicatedStorage").Basic.Anchor:FireServer(unpack(args))
-            local args = {
-                [1] = game:GetService("Players").LocalPlayer.Character["Left Arm"],
-                [2] = false
-            }
-            
-            game:GetService("ReplicatedStorage").Basic.Anchor:FireServer(unpack(args))
-            local args = {
-                [1] = game:GetService("Players").LocalPlayer.Character["Left Leg"],
-                [2] = false
-            }
-            
-            game:GetService("ReplicatedStorage").Basic.Anchor:FireServer(unpack(args))
-            local args = {
-                [1] = game:GetService("Players").LocalPlayer.Character["Right Arm"],
-                [2] = false
-            }
-            
-            game:GetService("ReplicatedStorage").Basic.Anchor:FireServer(unpack(args))
-            local args = {
-                [1] = game:GetService("Players").LocalPlayer.Character["Right Leg"],
-                [2] = false
-            }
-            
-            game:GetService("ReplicatedStorage").Basic.Anchor:FireServer(unpack(args))
-            
+            wait(0.0001)             
         end
     end
-        
-            function LightningSpam()
-                _G.LightningSpam = true do
-                game:GetService("ReplicatedStorage").SpecialMoves.HeavenlyLightningStrike:FireServer()
+    
+    function OHHeal()
+        while _G.OHHeal == true do
+            game:GetService("ReplicatedStorage").Attacks.Heal.OverHeavenHeal:FireServer(true,workspace.spenc12312.Humanoid,50,CFrame.new(-1679.73999, 2973.18652, -1958.71997, 0.318209827, -0.875269413, 0.364205956, -0.461814761, -0.478626251, -0.746755779, 0.827931166, 0.0694293529, -0.556515932),1,"rbxassetid://1202656211",2.5)
+                wait(.06)
+        end
+    end
+    -- Main Function: [Stand Storage] --
+    function StandStorage1()
+        while _G.StandStorage1 == true do
+            local args = {
+                [1] = 1
+            }
+            
+            game:GetService("ReplicatedStorage").Essentals.StandStorage:FireServer(unpack(args))  
+            wait(10000000000000000000000000000000000000)
+        end
+    end
+    function StandStorage2()
+        while _G.StandStorage2 == true do
+            local args = {
+                [1] = 2
+            }
+            
+            game:GetService("ReplicatedStorage").Essentals.StandStorage:FireServer(unpack(args))  
+            wait(10000000000000000000000000000000000000)
+        end
+    end
+    function StandStorage3()
+        while _G.StandStorage3 == true do
+            local args = {
+                [1] = 3
+            }
+            
+            game:GetService("ReplicatedStorage").Essentals.StandStorage:FireServer(unpack(args))  
+            wait(10000000000000000000000000000000000000)
+        end
+    end
+            -- Map teleports: Variables --
+            getgenv().TPPlace = "string"
+    
+    --- Stand Abilities ---
+    function InfiniteEpitaph()
+        while _G.InfiniteEpitaph == true do
+            local args = {
+                [1] = "Epitaph"
+            }
+            
+            game:GetService("Players").LocalPlayer.Backpack["King Crimson"].KCLocalisedRemote:FireServer(unpack(args))
+            wait(100)
+        end
+        end
+        function TheHandPull()
+            while _G.TheHandPull == true do
+                local args = {
+                    [1] = "Pull",
+                    [2] = 50
+                }
                 
-                game:GetService("ReplicatedStorage").CoolDowns:FireServer("Lightning Strikes","(H)",10)
-               wait(.0002)
-                end
+                game:GetService("Players").LocalPlayer.Backpack["The Hand"].THLocalisedRemote:FireServer(unpack(args))
+                wait(10000000000000000000)
+            end            
             end
     
     
-        -- Tabs
-    local NoCDTab = Window:MakeTab({
-            Name = "Main",
-            Icon = "rbxassetid://4483345998",
-            PremiumOnly = false
-        })
-    
-        --Toggles
-    NoCDTab:AddToggle({
-            Name = "CD off: Vampire spec/vtw",
-            Default = false,
-            Callback = function(Value)
-                _G.NoCoolDown = Value
-                NoCoolDown()
-            end    
-        })
-        NoCDTab:AddToggle({
-            Name = "Bypass TS",
-            Default = false,
-            Callback = function(Value)
-                _G.BypassTS = Value
-                BypassTS()
-            end    
-        })
-    
-        --Buttons
-        NoCDTab:AddButton({
-            Name = "Lightning Spam!",
-            Callback = function(Value)
-                  _G.LightningSpam = Value
-                  LightningSpam()
-              end    
-        })
+        function LightningSpam()
+            while _G.LightningSpam == true do
+                game:GetService("ReplicatedStorage").SpecialMoves.HeavenlyLightningStrike:FireServer()
+                wait(.002)
+            end
+        end
+        function SpaceRipper()
+            while _G.SpaceRipper == true do
+                local args = {
+                    [1] = nil --[[Vector3]]
+                }
+                
+                game:GetService("ReplicatedStorage").Attacks.SpaceRipper:FireServer(unpack(args))
+                    wait(.0005)            
+            end
+        end
         
-          
-        ------------------------------- Next Section can add more above if wanted to.. ---------------------------------------
-        
-        
-        --Another Tab
-        local NoCDTab = Window:MakeTab({
-            Name = "Other",
-            Icon = "rbxassetid://4483345998",
-            PremiumOnly = false
-        }) 
+        function TW20SECTS()
+            while _G.TW20SECTS == true do
+                local args = {
+                    [1] = 21,
+                    [2] = "DIO"
+                }
+                
+                game:GetService("ReplicatedStorage").SpecialMoves.Timestop:FireServer(unpack(args))
+                    wait(10000000000000)
+            end
+        end
     
+        function KnifeSpam()
+            while _G.KnifeSpam == true do
+                game:GetService("ReplicatedStorage").Attacks.DioKnifeThrow:FireServer(CFrame.new(-893.737183, -3.70075226, 1654.43665, 0.545864761, -2.96950393e-08, -0.83787328, -5.44422711e-08, 1, -7.09094792e-08, 0.83787328, 8.43227141e-08, 0.545864761))
+                    wait(.00000002)
+                end
+            end
+        
+        
+            -- Visuals --
+        function WSVisual()
+            while _G.WSVisual == true do
+                local args = {
+                    [1] = true,
+                    [2] = game:GetService("Players").LocalPlayer.Character.Stand["Stand Torso"]
+                }
+                
+                game:GetService("ReplicatedStorage").Attacks.Whitesnake.AcidSleep:FireServer(unpack(args))
+                    wait(.3)
+            end
+        end    
+    
+            -- Arrow grind --
+            function Getarrow()
+                while _G.Getarrow == true do
+                    game:GetService("ReplicatedStorage").ItemGiver.GiveArrow:FireServer()
+                        wait(10000000000000000000000000000000000000000000000000000000000000000000000000)
+                end
+            end
+        ---------------------------------
+    local Tab1 = Window:MakeTab({
+        Name = "Main",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+    local Section = Tab1:AddSection({
+        Name = "Immune to Time Stop"
+    })
+    Tab1:AddToggle({
+        Name = "Bypass TS",
+        Default = false,
+        Callback = function(Value)
+            _G.BypassTS = Value
+            BypassTS()
+        end    
+    })
+    local Section = Tab1:AddSection({
+        Name = "Troll"
+    })
+    Tab1:AddToggle({
+        Name = "Demi-God",
+        Default = false,
+        Callback = function(Value)
+            _G.InfiniteBlock = Value
+            InfiniteBlock()
+        end    
+    })
+    Tab1:AddToggle({
+        Name = "Heal",
+        Default = false,
+        Callback = function(Value)
+            _G.OHHeal = Value
+            OHHeal()
+        end    
+    })
+    local Section = Tab1:AddSection({
+        Name = "StandStorage"
+    })
+    Tab1:AddButton({
+        Name = "Storage1",
+        Callback = function()
+            StandStorage1()
+          end    
+    })
+    Tab1:AddButton({
+        Name = "Storage2",
+        Callback = function()
+            StandStorage2()
+          end    
+    })
+    Tab1:AddButton({
+        Name = "Storage3",
+        Callback = function()
+            StandStorage3()
+          end    
+    })
+    local Tab2 = Window:MakeTab({
+        Name = "Map Teleport",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+    
+    
+    local Section = Tab2:AddSection({
+        Name = "Teleport to Selected Area"
+    })
+    Tab2:AddDropdown({
+        Name = "Teleports",
+        Default = "1",
+        Options = {"Zone 1: Arrow Shop", "Zone 2: Health Dummies", "Zone 3: Punching Dummy", "Zone 4: Zeppeli"},
+        Callback = function(Value)
+            getgenv().TPPlace = Value
+        end    
+    })
+    Tab2:AddButton({
+        Name = "Teleport",
+        Callback = function()
+            if getgenv().TPPlace == "Zone 1: Arrow Shop" then
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-626.592224, -7.8500061, 1704.08252, -0.803138137, 0, -0.595793545, 0, 1, 0, 0.595793545, 0, -0.803138137)
+            elseif getgenv().TPPlace == "Zone 2: Health Dummies" then
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-799.729675, -4.81324768, 1614.57605, -0.992935896, 0, 0.118655324, 0, 1, 0, -0.118655324, 0, -0.992935896)
+            elseif getgenv().TPPlace == "Zone 3: Health Dummies" then
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(519.117126, 62.9999886, 2333.86035, -0.973355949, -5.94263616e-09, 0.229299396, -5.00248065e-09, 1, 4.68139794e-09, -0.229299396, 3.4096006e-09, -0.973355949)
+                
+            elseif  getgenv().TPPlace == "Zone 4: Zeppeli" then
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-697.489258, 41.5688553, 1459.75476, -0.989194512, 0, 0.14660874, 0, 1, 0, -0.14660874, 0, -0.989194512)
+    
+            end
+        end  
+    })
+    
+    local Tab3 = Window:MakeTab({
+        Name = "Stand Abilities",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+    local Section = Tab3:AddSection({
+        Name = "King Crimson:"
+    })
+    Tab3:AddToggle({
+        Name = "Infinite Epitaph",
+        Default = false,
+        Callback = function(Value)
+            _G.InfiniteEpitaph = Value
+            InfiniteEpitaph()
+        end    
+    })
+    local Section = Tab3:AddSection({
+        Name = "The Hand:"
+    })
+    Tab3:AddButton({
+        Name = "Space Pull",
+        Callback = function()
+              TheHandPull()
+          end    
+    })
+    local Section = Tab3:AddSection({
+        Name = "The Word Over Heaven:"
+    })
+    Tab3:AddToggle({
+        Name = "Lightning Spam",
+        Default = false,
+        Callback = function(Value)
+            _G.LightningSpam = Value
+            LightningSpam()
+        end    
+    })
+    local Section = Tab3:AddSection({
+        Name = "Vampire Spec/VTW:"
+    })
+    Tab3:AddToggle({
+        Name = "Space Ripper",
+        Default = false,
+        Callback = function(Value)
+            _G.SpaceRipper = Value
+            SpaceRipper()
+        end    
+    })
+    local Section = Tab3:AddSection({
+        Name = "The World:"
+    })
+    Tab3:AddButton({
+        Name = "20 Second Time Stop",
+        Callback = function()
+                TW20SECTS()
+          end    
+    })
+    Tab3:AddToggle({
+        Name = "Knife Spam",
+        Default = false,
+        Callback = function(Value)
+            _G.KnifeSpam = Value
+            KnifeSpam()
+        end    
+    })
+    local Tab4 = Window:MakeTab({
+        Name = "Visuals",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+    local Section = Tab4:AddSection({
+        Name = "WhiteSnake Visuals"
+    })
+    Tab4:AddToggle({
+        Name = "Sleep Effect",
+        Default = false,
+        Callback = function(Value)
+            _G.WSVisual = Value
+            WSVisual()
+        end    
+    })
+    local Tab4 = Window:MakeTab({
+        Name = "Auto farm (Kinda)",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+    local Section = Tab4:AddSection({
+        Name = "Buy Arrow (at your own risk)"
+    })
+    Tab4:AddButton({
+        Name = "Get an Arrow [WILL TAKE ALL YOUR MONEY]",
+        Callback = function(Value)
+                Getarrow()
+          end    
+    })
+    local Tab5 = Window:MakeTab({
+        Name = "Credits",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+    Tab5:AddParagraph("This Script was made by","! Ace#3790 (add on discord if you want to)")
+    
+    Tab5:AddParagraph("Shoutouts to:","Hikari for the ideas!")
+    
+    Tab5:AddParagraph("Shoutout:","Jojo for the support!")
+    
+    Tab5:AddParagraph("Shoutout:","Goku for the support and being cool!")
+    
+    local Tab6 = Window:MakeTab({
+        Name = "Changelogs",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+    local Section = Tab6:AddSection({
+        Name = "Changelogs | v0.4"
+    })
+    Tab6:AddParagraph("Added: Bypass TS","[+]")
+    Tab6:AddParagraph("Fixing: Arrow grind","[/]")
+    Tab6:AddParagraph("Fixing: Spam stand moves (bugged)","[-]")
+    Tab6:AddParagraph("Added: God mode (Heal with TW:OH)","[+]")
+    Tab6:AddParagraph("Removed: Heal all (broken)","[-]")
+    local Section = Tab6:AddSection({
+        Name = "Changelogs | v0.5"
+    })
+    Tab6:AddParagraph("Added: Infinite Block","[+]")
+    Tab6:AddParagraph("Added: Visuals","[+]")
+    Tab6:AddParagraph("Added: Teleport to zones","[+]")
+    Tab6:AddParagraph("Added: StandStorage (portable)","[+]")
+    Tab6:AddParagraph("MORE COMING SOON!!","")
     
     
     end
-    ---------- KEY SYSTEM -------------------
-    
-    
-    
     OrionLib:Init()
-end 
-
-function CorrectKeyNotification()
-    OrionLib:MakeNotification({
-        Name = "Less gooo",
-        Content = "You've Entered Key Correctly",
-        Image = "rbxassetid://4483345998",
-        Time = 5
-    })
-end
-
-function IncorrectKeyNotification()
-    OrionLib:MakeNotification({
-        Name = "Invalid Key",
-        Content = "The Key You've entered is Incorrect.. Try again!",
-        Image = "rbxassetid://4483345998",
-        Time = 5
-    })
-end
-
-
-local Tab = Window:MakeTab({
-	Name = "Key",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-
-
-Tab:AddTextbox({
-	Name = "Enter Key",
-	Default = "",
-	TextDisappear = true,
-	Callback = function(Value)
-        _G.KeyInput = Value
-	end	  
-})
-
-Tab:AddButton({
-	Name = "Check Key",
-	Callback = function()
-      		if _G.KeyInput == _G.Key then
-            MakeScriptHub()
-            CorrectKeyNotification()
-            else
-                IncorrectKeyNotification()
-            end
-        end    
-})
+    
